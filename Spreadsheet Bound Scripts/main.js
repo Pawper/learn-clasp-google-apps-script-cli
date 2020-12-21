03b6dc852c124d11927df7d2a97ed9b48e5728ac
Pawper <phillip.a.wessels@gmail.com>
2020-12-20 18:07:51 -0800

    Lesson 56 complete

--- Spreadsheet Bound Scripts/main.js
+++ Spreadsheet Bound Scripts/main.js
@@ -1,39 +1,52 @@
-const GLVAL = 'Testing Global Value';
-let COUNTER = 0;
-
-function onOpen() {
-  PropertiesService.getDocumentProperties().setProperty('cnt', 0)
-  const ui = SpreadsheetApp.getUi();
-  ui.createMenu("adv")
-    .addItem('showSide1', 'side1')
-    .addItem('showSide2', 'side2')
-    .addItem('showSide3', 'side3')
-    .addToUi();
+function SELVALA1 () {
+  return SpreadsheetApp.getActive().getActiveRange().getA1Notation();
 }
 
-function side1() {
-  const ui = SpreadsheetApp.getUi();
-  const html = HtmlService.createHtmlOutput('<h1>Hello World</h1><p>Tested</p>');
-  ui.showSidebar(html);
-}
-function side2() {
-  const ui = SpreadsheetApp.getUi();
-  const html = HtmlService.createHtmlOutputFromFile('temp');
-  ui.showSidebar(html);
-}
-function side3() {
-  COUNTER = PropertiesService.getDocumentProperties().getProperty('cnt');
-  COUNTER++;
-  PropertiesService.getDocumentProperties().setProperty('cnt', COUNTER)
-  const ui = SpreadsheetApp.getUi();
-  const html = HtmlService.createTemplateFromFile('temp1').evaluate();
-  ui.showSidebar(html);
+function addForm() {
+  const ss = SpreadsheetApp.getActive();
+  const sheet = ss.getSheetByName('test');
+  const range = sheet.getRange('C1:C5');
+  range.setFormula('=SUM(A1:B1)');
+  range.setFontColor('red');
+  range.setBackground('pink');
 }
 
-function logVal(val) {
-  const ss = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("log");
-  ss.appendRow([val]);
-}
+// const GLVAL = 'Testing Global Value';
+// let COUNTER = 0;
+
+// function onOpen() {
+//   PropertiesService.getDocumentProperties().setProperty('cnt', 0)
+//   const ui = SpreadsheetApp.getUi();
+//   ui.createMenu("adv")
+//     .addItem('showSide1', 'side1')
+//     .addItem('showSide2', 'side2')
+//     .addItem('showSide3', 'side3')
+//     .addToUi();
+// }
+
+// function side1() {
+//   const ui = SpreadsheetApp.getUi();
+//   const html = HtmlService.createHtmlOutput('<h1>Hello World</h1><p>Tested</p>');
+//   ui.showSidebar(html);
+// }
+// function side2() {
+//   const ui = SpreadsheetApp.getUi();
+//   const html = HtmlService.createHtmlOutputFromFile('temp');
+//   ui.showSidebar(html);
+// }
+// function side3() {
+//   COUNTER = PropertiesService.getDocumentProperties().getProperty('cnt');
+//   COUNTER++;
+//   PropertiesService.getDocumentProperties().setProperty('cnt', COUNTER)
+//   const ui = SpreadsheetApp.getUi();
+//   const html = HtmlService.createTemplateFromFile('temp1').evaluate();
+//   ui.showSidebar(html);
+// }
+
+// function logVal(val) {
+//   const ss = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("log");
+//   ss.appendRow([val]);
+// }
 
 // function onOpen() {
 //   const ui = SpreadsheetApp.getUi();
